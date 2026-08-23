aa25206de9a0d003f1871ec960a01cb3857b5d61
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-23 14:26:32 +0330

    feat: add the get and create reservationApi

--- src/entities/reservation/api/reservation-api.ts
+++ src/entities/reservation/api/reservation-api.ts
@@ -1,3 +1,6 @@
+import { httpClient } from "@/shared/api/http-client";
+import { Reservation, ReservationInput } from "../model/types";
+
 type ApiResponse<T> = {
     data:T
 }
@@ -5,5 +8,17 @@ type ApiResponse<T> = {
 const RESERVATIONS_URL = "/api/reservations";
 
 export const reservationApi = {
-    
+    async getById(id:string):Promise<Reservation>{
+        const response = await httpClient.get<ApiResponse<Reservation>>(
+            `${RESERVATIONS_URL}/${id}`
+        )
+        return response.data
+    },
+
+    async create(input:ReservationInput):Promise<Reservation>{
+        const response = await httpClient.patch<ApiResponse<Reservation>>(
+            RESERVATIONS_URL , input
+        )
+        return response.data
+    }
 }
