f6ecb54945c4f72ca3388a35a85fc4adffcef2b9
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-23 14:28:55 +0330

    feat: add the remove and update reservation api function

--- src/entities/reservation/api/reservation-api.ts
+++ src/entities/reservation/api/reservation-api.ts
@@ -1,24 +1,45 @@
 import { httpClient } from "@/shared/api/http-client";
-import { Reservation, ReservationInput } from "../model/types";
+import {
+  Reservation,
+  ReservationInput,
+  ReservationUpdateInput,
+} from "../model/types";
 
 type ApiResponse<T> = {
-    data:T
-}
+  data: T;
+};
 
 const RESERVATIONS_URL = "/api/reservations";
 
 export const reservationApi = {
-    async getById(id:string):Promise<Reservation>{
-        const response = await httpClient.get<ApiResponse<Reservation>>(
-            `${RESERVATIONS_URL}/${id}`
-        )
-        return response.data
-    },
+  async getById(id: string): Promise<Reservation> {
+    const response = await httpClient.get<ApiResponse<Reservation>>(
+      `${RESERVATIONS_URL}/${id}`,
+    );
+    return response.data;
+  },
 
-    async create(input:ReservationInput):Promise<Reservation>{
-        const response = await httpClient.patch<ApiResponse<Reservation>>(
-            RESERVATIONS_URL , input
-        )
-        return response.data
-    }
-}
+  async create(input: ReservationInput): Promise<Reservation> {
+    const response = await httpClient.patch<ApiResponse<Reservation>>(
+      RESERVATIONS_URL,
+      input,
+    );
+    return response.data;
+  },
+
+  async update(
+    id: string,
+    input: ReservationUpdateInput,
+  ): Promise<Reservation> {
+    const response = await httpClient.patch<ApiResponse<Reservation>>(
+      `${RESERVATIONS_URL}/${id}`,
+      input,
+    );
+
+    return response.data;
+  },
+
+  async remove(id: string): Promise<void> {
+    await httpClient.delete<ApiResponse<null>>(`${RESERVATIONS_URL}/${id}`);
+  },
+};
